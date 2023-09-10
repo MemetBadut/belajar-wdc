d11c3c3ffa14a587a4b6b836bcdd3ff6e5d78774
Memet Cibadut <pasekm06@gmail.com>
2023-09-10 10:52:12 +0800

    anak

--- untukpemula/index.php
+++ untukpemula/index.php
@@ -1,27 +1,29 @@
-<?php 
+<?php
 
-if(isset($_POST['simpan'])){
+if (isset($_POST['simpan'])) {
     $nilai = $_POST['nilai'];
-    if($nilai > 90){
+    if ($nilai > 90) {
         echo "Grade A";
-    }elseif($nilai > 80){
+    } elseif ($nilai > 80) {
         echo "Grade B";
-    }elseif($nilai >= 75){
+    } elseif ($nilai >= 75) {
         echo "Grade C";
-    }else{
+    } else {
         echo "Anda tidak lulus";
     }
 }
-?> 
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar Kebatinan</title>
 </head>
+
 <body>
     <form method="post">
         <label for="">Nilai</label>
@@ -29,4 +31,5 @@ if(isset($_POST['simpan'])){
         <button type="submit" value="simpan" name="simpan">Simpan</button>
     </form>
 </body>
+
 </html>

ac3073189500aaa56a54319af48a47f8d50e161e
Memet Cibadut <pasekm06@gmail.com>
2023-09-10 09:45:05 +0800

    jadi pemula

---
+++ login-form/admin_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Admin Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h3>Hi, <span>admin</span></h3>
+            <h1>Welocme <span></span></h1>
+            <p>this is a Admin Page</p>
+            <a href="login_form.php" class="btn">Login</a>
+            <a href="register_form.php"></a>
+        </div>
+    </div>
+</body>
+</html>

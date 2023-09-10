e00c1e1f8b6829902a1c91103c3ce6fe7ba266c4
Memet Cibadut <pasekm06@gmail.com>
2023-09-10 10:45:37 +0800

    jadi sepuh

--- untukpemula/index.php
+++ untukpemula/index.php
@@ -1,51 +1,32 @@
 <?php 
 
-// $uts = 10;
-// if($uts > 9){
-//     echo "anda lulus";
-// }else{
-//     echo "anda tidak lulus yahahaa hayuk";
-// }
-
-// echo "<br>";
-
-// $nilai = 8;
-// if($uts > 9){
-//     echo "anda mendapat nilai A";
-// }elseif($nilai > 8){
-//     echo "anda mendapat nilai B";
-// }
-
-// $level = 2;
-// switch($level){
-//     case 1:
-//         echo "Anda di level pemula";
-//         break;
-//     case 2:
-//         echo "Anda di level beginer";
-//         break;
-//     case 3:
-//         echo "Anda di level expert";
-//         break;
-//     default:
-//     echo "Anda berada di ruangan Antah Beranta";
-// }
-
-
-// $suka = true;
-// echo $suka ? "aku suka makan ayam goreng" : "aku endak suka makan ayam goreng";
-$kkm_uas = 80;
-$kkm_uts = 75;
-
-
-if ($kkm_uas >= 85) {
-    echo "anda lulus";
-}else{
-    if($kkm_uts >= 75){
-        echo "Anda lulus mendapat bantuan nilai uts";
+if(isset($_POST['simpan'])){
+    $nilai = $_POST['nilai'];
+    if($nilai > 90){
+        echo "Grade A";
+    }elseif($nilai > 80){
+        echo "Grade B";
+    }elseif($nilai >= 75){
+        echo "Grade C";
     }else{
-        echo "anda tidak lulus";
+        echo "Anda tidak lulus";
     }
 }
+?> 
 
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Kebatinan</title>
+</head>
+<body>
+    <form method="post">
+        <label for="">Nilai</label>
+        <input type="number" name="nilai">
+        <button type="submit" value="simpan" name="simpan">Simpan</button>
+    </form>
+</body>
+</html>
